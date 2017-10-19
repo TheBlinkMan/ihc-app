50d9d83aa5ea971bf31b8f703556ec52270a757c
TheBlinkMan <cardosoalanf@gmail.com>
2017-10-18 22:51:13 -0200

    Fix the configuration and Add the SERVER_NAME variable
    Change all the possible configurations so that the database will be created
    in the same folder as the project.
    Add the SERVER_NAME variable, this will help the url_for function to
    find the correct url in the project's blueprints.

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SERVER_NAME = 'localhost:5000'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
@@ -13,18 +14,18 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    db_path = os.path.join(basedir, '../data-dev.sqlite')
+    db_path = os.path.join(basedir, 'data-dev.sqlite')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
             'sqlite:///' + db_path
 
 class TestingConfig(Config):
     TESTING = True
-    db_path = os.path.join(basedir, '../data-test.sqlite')
+    db_path = os.path.join(basedir, 'data-test.sqlite')
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
             'sqlite:///' + db_path
 
 class ProductionConfig(Config):
-    db_path = os.path.join(basedir, '../data.sqlite')
+    db_path = os.path.join(basedir, 'data.sqlite')
     SQLALCHEMY_DATABASE_URI = os.environ.get('PRODUCTION_DATABASE_URL') or \
             'sqlite:///' + db_path
 
