17efedf4e2f0ca7be13da7b5d97fbb870845e6a6
TheBlinkMan <cardosoalanf@gmail.com>
2017-10-19 00:36:30 -0200

    Add the cli and the shell and test commands, Add only_json function
    Add the command line interface.
    Add the shell command so that we can test the application within
    a shell with the application context.
    Add the test command so we can use and see the results of the tests
    Add the only_json function that will be executed before every request
    so the developers don't need to test if the request in the views that
    have POST and PUT as methods have the application/json value in the
    header.

--- webapp.py
+++ webapp.py
@@ -1,8 +1,23 @@
 import os
-from app import create_app
+from app import create_app, db
+from app.models import User
+from flask import request, abort
+import click
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.before_request
+def only_json():
+    if request.method in ['POST', 'PUT'] and not request.is_json:
+        abort(400)
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(app=app, db=db, User=User)
+
+@app.cli.command()
+@click.option('--coverage/--no-coverage', default=False, help='Enable code coverage')
+def test(coverage):
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
