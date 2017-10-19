3cbd48594e949cd9c65304add2f66de241eb4d23
TheBlinkMan <cardosoalanf@gmail.com>
2017-10-19 00:25:10 -0200

    Add the api blueprint and Add the extensions to the project
    Add the api blueprint and register the blueprint within the application.
    Add the extensions instantiating their respective objects.

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,22 @@
 from flask import Flask
 from config import config
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, current_user
+from flask_httpauth import HTTPTokenAuth
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+auth = HTTPTokenAuth(scheme='Bearer')
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    from .api_1_0 import api as api_1_0_blueprint
+    app.register_blueprint(api_1_0_blueprint, url_prefix='/api/v1.0')
 
     return app
