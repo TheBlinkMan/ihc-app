022d6ab0fda936f64babcf3d700a09470666dcc7
TheBlinkMan <cardosoalanf@gmail.com>
2017-12-02 01:48:53 -0200

    Add flask_migrate extension, Update requirements.txt

--- webapp.py
+++ webapp.py
@@ -3,8 +3,10 @@ from app import create_app, db
 from app.models import User, Role
 from flask import request, abort
 import click
+from flask_migrate import Migrate
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+migrate = Migrate(app, db)
 
 @app.before_request
 def only_json():
